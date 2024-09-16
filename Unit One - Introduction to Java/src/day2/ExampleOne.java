04f92d3f375f34571bc25ac61c126bc3b5e70952
Daniel Pang <dpang@bayviewglen.ca>
2024-09-16 09:26:39 -0400

    day2

---
+++ Unit One - Introduction to Java/src/day2/ExampleOne.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class ExampleOne {
+    public static void main(String[] args) {
+
+        int number = 3054;
+
+        int digit1 = number / 1000;
+
+        int digit2 = number / 100 % 10;
+
+        int digit3 = number / 10 % 10;
+
+        int digit4 = number % 10;
+
+        System.out.println(digit1 + digit2 + digit3 + digit4);
+
+    }
+    
+}
---
+++ Unit One - Introduction to Java/src/day2/ExampleTwo.java
@@ -0,0 +1,37 @@
+package day2;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        int x = 4, y = 7;
+        double a = 6.4, b = 7.9;
+        double c = 1.0;
+
+        int sum = x + y;
+
+        int p = y%2;
+
+        p = y/2;
+
+        double mult = a * b;
+
+        x = x - 1;   
+
+        int numStudents = 14;
+
+        numStudents = numStudents + 1;
+        numStudents++;
+        numStudents--;
+
+        numStudents += 3;
+        numStudents-=2;
+
+        double temperature = 12.9;
+
+        temperature *= 2;
+
+        temperature /= 2;
+
+        x %= 2;
+    }
+    
+}
